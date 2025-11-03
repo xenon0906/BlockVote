94338361076e767b0e3e91c8d4f4e9d7fa8024d5
xenon0906 <150368103+xenon0906@users.noreply.github.com>
2025-11-04 00:24:07 +0530

    fix next.js config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   compress: true,
   poweredByHeader: false,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    config.externals.push('pino-pretty', 'lokijs', 'encoding');
-    return config;
-  },
-  experimental: {
-    optimizeCss: true,
+  turbopack: {
+    resolveExtensions: ['.tsx', '.ts', '.jsx', '.js'],
   },
 }
 
